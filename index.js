5ec79552cfb6d1206a7e87a8009cbc38ffc5965e
ShaheerKz <shariikhan925@gmail.com>
2023-06-07 14:24:34 +0500

    feat: chnage this navbar design

---
+++ index.js
@@ -0,0 +1,10 @@
+// const open_menu = document.querySelector(".fa-bars");
+// const close_menu = document.querySelector(".fa-square-xmark");
+// const nav_active = document.querySelectorAll(".navbar");
+
+// open_menu.addEventListener("click", () => {
+//   nav_active.forEach((Element) => Element.classlist.add("active"));
+// });
+// open_menu.addEventListener("click", () => {
+//   nav_active.forEach((Element) => Element.classlist.remove("active"));
+// });
